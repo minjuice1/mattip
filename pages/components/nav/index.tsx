1d792b632c7594671310fc7ad8251c045c54e76b
minjuice1 <dev.minju@gmail.com>
2023-03-02 23:58:47 +0900

    chore / main_branch / 초기 세팅 - 디렉토리 구조 설계

---
+++ pages/components/nav/index.tsx
@@ -0,0 +1,7 @@
+import styles from "./index.module.css";
+
+const Nav = () => {
+	return <div className={styles.container}></div>;
+};
+
+export default Nav;
---
+++ pages/login/index.tsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+	return (
+		<div>
+			<h1>this is LoginPage</h1>
+		</div>
+	);
+};
+
+export default LoginPage;
