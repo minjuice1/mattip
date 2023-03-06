7269c5fed9376cd266935335de4ca7a065c1a913
minjuice1 <dev.minju@gmail.com>
2023-03-06 21:10:51 +0900

    chore / style : header 컴포넌트 - 클릭 시 메인페이지로 이동

--- src/components/common/card/index.tsx
+++ src/components/common/card/index.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from "react";
-import styles from "./card.module.css";
+import styles from "./index.module.css";
 
 const Card = (props: PropsWithChildren) => {
 	return (
--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import styles from "./index.module.css";
 
 const Header = () => {
 	return (
 		<header className={styles.container}>
 			<div className={styles.logoBox}>
-				<span className={styles.logo}>Mat Tip</span>
+				<Link href='/' className={styles.logo}>
+					Mat Tip
+				</Link>
 			</div>
 		</header>
 	);
