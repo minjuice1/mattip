903d93bd6e8e4149aaec6b53c102b4deb23b5413
minjuice1 <dev.minju@gmail.com>
2023-03-09 11:55:51 +0900

    remove, conf / style : 전체 레시피 페이지 - 라이브러리 변경 & 현재 쓰지않는 폴더 삭제 / #11

--- src/pages/recipe/all/index.tsx
+++ src/pages/recipe/all/index.tsx
@@ -1,26 +1,15 @@
 import AllRecipeCard from "@/src/components/allRecipesCard";
-import Masonry from "react-masonry-css";
+import Masonry from "react-responsive-masonry";
 import Img1 from "@/src/images/img1.jpg";
 import styles from "./index.module.css";
 
 const AllRecipePage = () => {
 	const list = Array.from({ length: 10 }, (_, i) => i);
 
-	const breakpointColumnsObj = {
-		default: 3,
-		1100: 2,
-		700: 1,
-		500: 1,
-	};
-
 	return (
 		<>
 			<div className={styles.container}>
-				<Masonry
-					breakpointCols={breakpointColumnsObj}
-					className='my-masonry-grid'
-					columnClassName='my-masonry-grid_column'
-				>
+				<Masonry columnsCount={3}>
 					{list.map((_, i) => (
 						<AllRecipeCard key={i} image={Img1} />
 					))}
