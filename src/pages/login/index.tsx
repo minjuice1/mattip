ad7403e18d0049343f58f45b1f98ce4a65558547
minjuice1 <dev.minju@gmail.com>
2023-03-11 12:55:51 +0900

    style, format / style : 전체 페이지 - 레시피 페이지 route 변경 & 모든 페이지 커서, 간격, 컬러 수정 / #11

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -5,8 +5,16 @@ const LoginPage = () => {
 	return (
 		<>
 			<h1 className={styles.title}>MatTip</h1>
-			<LoginBtn text={"카카오 로그인"} bottom={270} />
-			<LoginBtn text={"구글 로그인"} bottom={210} />
+
+			<h2 className={styles.subTitle}>
+				<span> 맛팁과 함께</span>
+				<span>
+					<span className={styles.strong}>맛있는 레시피</span>를
+				</span>
+				<span> 만들어보세요!</span>
+			</h2>
+			<LoginBtn text={"카카오 로그인"} bottom={235} />
+			<LoginBtn text={"구글 로그인"} bottom={165} />
 		</>
 	);
 };
